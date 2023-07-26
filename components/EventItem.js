29112226458cf1cd964b314ba9f4f7a8082335f4
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-26 16:31:15 +0100

    rendering individual events

---
+++ components/EventItem.js
@@ -0,0 +1,44 @@
+
+import classes from "./EventItem.module.css"
+import Button from "../ui/Button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon"
+
+const EventItem = ({image, title, date, location, id}) => {
+
+    const humanReadableTime = new Date(date).toLocaleString("en-US", {day: "numeric", month: "long", year: "numeric"})
+    const formatLocation = location.replace(",", "\n")
+    return (
+        <li className={classes.item}>
+            <img src={`/${image}`} alt={title} />
+            <div className={classes.content}>
+                <div>
+                    <div className={classes.summary}>
+                        <h2>{title}</h2>
+                    </div>
+                    <div className={classes.date}>
+                        <DateIcon />
+                        <time>{humanReadableTime}</time>
+                    </div>
+                    <div className={classes.address}>
+                        <AddressIcon />
+                        <address>{formatLocation}</address>
+                    </div>
+                </div>
+                <div className={classes.actions}>
+                    <Button link={`/events/${id}`}>
+                    <span>Explore Events</span>
+                    <span className={classes.icon}>
+                        <ArrowRightIcon />
+                    </span>
+                    </Button>
+                    
+                
+            </div>
+            </div>
+        </li>
+    )
+}
+
+export default EventItem
