fdcce34aa55455a4494c158b1f318437df50b369
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-08-05 00:27:50 +0100

    static site page pre rendering

--- pages/events/index.js
+++ pages/events/index.js
@@ -4,19 +4,45 @@ import { getAllEvents } from "../../dummy_data";
 import EventSearch from "../../components/EventSearch";
 import { useRouter } from "next/router";
 
-const AllEventsPage = () => {
+const AllEventsPage = (props) => {
 
+    const {events} = props
     const router = useRouter()
-    const allEvents = getAllEvents();
+    // const allEvents = getAllEvents();
     const searchEventHandler = (month, year) => {
         router.push("/events/" + year + "/" + month)
     }
     return (
         <Fragment>
             <EventSearch onSearch={searchEventHandler} />
-        <EventList items={allEvents} />
+        <EventList items={events} />
         </Fragment>
     )
+}
+
+export const getStaticProps = async () => {
+
+    const response = await fetch("https://nextjs-e65fb-default-rtdb.firebaseio.com/events.json");
+    const resData = await response.json();
+    const transformedEvents = [];
+    for( const key in resData ) {
+        transformedEvents.push({
+            id: key,
+            title: resData[key].title,
+            date: resData[key].date,
+            description: resData[key].description,
+            location: resData[key].location,
+            image: resData[key].image,
+            isFeatured: resData[key].isFeatured
+        })
+    }
+
+    
+    return {
+        props: {
+            events: transformedEvents
+        }
+    }
 }
 
 export default AllEventsPage;
