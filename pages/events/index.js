9fb045c71fad01e2ac7bc9ecb7c350e663bc88a2
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-08-15 17:35:14 +0100

    all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -4,6 +4,7 @@ import EventList from "../../components/EventList";
 import EventSearch from "../../components/EventSearch";
 import { useRouter } from "next/router";
 import Head from "next/head"
+import { getAllEvents } from "../../helpers/apiUtil";
 
 const AllEventsPage = (props) => {
 
@@ -27,20 +28,7 @@ const AllEventsPage = (props) => {
 
 export const getStaticProps = async () => {
 
-    const response = await fetch("https://nextjs-e65fb-default-rtdb.firebaseio.com/events.json");
-    const resData = await response.json();
-    const transformedEvents = [];
-    for( const key in resData ) {
-        transformedEvents.push({
-            id: key,
-            title: resData[key].title,
-            date: resData[key].date,
-            description: resData[key].description,
-            location: resData[key].location,
-            image: resData[key].image,
-            isFeatured: resData[key].isFeatured
-        })
-    }
+    const transformedEvents = await getAllEvents();
 
     
     return {
