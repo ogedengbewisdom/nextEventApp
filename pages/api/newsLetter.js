700c4d69e007d8de8ab81b4538e48ff0d4a05046
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-09-21 15:40:41 +0100

    handling error and showing notification

--- pages/api/newsLetter.js
+++ pages/api/newsLetter.js
@@ -9,8 +9,14 @@ const handler = async (req, res) => {
             res.status(422).json({message: "Invalid email address"})
             return;
         }
-
-        const client = await MongoClient.connect("mongodb+srv://onimisi:wisdom123456@cluster0.pheshy5.mongodb.net/event?retryWrites=true&w=majority")
+        let client;
+        try {
+            client = await MongoClient.connect("mongodb+srv://onimisi:wisdom123456@cluster0.pheshy5.mongodb.net/event?retryWrites=true&w=majority")
+        } catch(error) {
+            res.status(500).json({message: "wrong connection"});
+            return
+        }
+        
         const db = client.db();
         await db.collection("newsletter").insertOne({email: email})
         client.close()
