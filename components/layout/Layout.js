037e87aee0f6d0c0a6a1bdfd7907073e44f24ef5
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-09-21 15:39:11 +0100

    useContext for app wide state management

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,11 +1,17 @@
-import { Fragment } from "react"
+import { Fragment, useContext } from "react"
 import Mainheader from "./Mainheader"
+import Notification from "../../ui/notification"
+import NotificationCtx from "../../store/notification-context"
 
 const Layout = (props) => {
+
+  const notificationCtx = useContext(NotificationCtx)
+  const activeNotification = notificationCtx.notification
    return (
      <Fragment>
         <Mainheader />
         <main>{props.children}</main>
+        {activeNotification && <Notification title={activeNotification.title} status={activeNotification.status} message={activeNotification.message} />}
     </Fragment>)
 }
 
