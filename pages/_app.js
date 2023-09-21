5f10508e26006ac1a6ca0c90338063f389cc15a7
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-09-21 15:40:07 +0100

    wrapping root app with context provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,22 @@
 import '../styles/globals.css';
 import Layout from '../components/layout/Layout';
 import Head from "next/head";
+import { NotificationProvider } from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NextJs Events.' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+
+      <NotificationProvider>
+          <Layout>
+            <Head>
+              <title>Next Events</title>
+              <meta name='description' content='NextJs Events.' />
+              <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+            </Head>
+            <Component {...pageProps} />
+          </Layout>
+      </NotificationProvider>
+      
   );
 }
 
